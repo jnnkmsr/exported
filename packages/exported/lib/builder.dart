5c31b90e86c88413f3195a26a3b0e8e0e7dd1ad5
Jannik Möser <dev@jannik-moeser.de>
2025-02-18 23:52:06 +0100

    feat: enable `CacheBuilder` in `build.yaml`

--- packages/exported/lib/builder.dart
+++ packages/exported/lib/builder.dart
@@ -1,5 +1,19 @@
 import 'package:build/build.dart';
+import 'package:exported/src/builder/export_cache_generator.dart';
 import 'package:exported/src/builder/exported_builder.dart';
+import 'package:exported_annotation/exported_annotation.dart';
 
-/// Generates Dart barrel files from annotated elements and builder [options].
+/// Reads intermediate JSON containing elements annotated with [Exported] and
+/// generates the barrel files, taking into account the builder [options].
+///
+/// Needs [cacheBuilder] to be run first to generate the intermediate JSON into
+/// the build cache.
 Builder exportedBuilder(BuilderOptions options) => ExportedBuilder(options);
+
+/// Collects elements annotated with [Exported] and stores them as JSON into
+/// the build cache.
+///
+/// This allows [exportedBuilder] to run without using a [Resolver], which
+/// would invalidate output on any change of the target package or any of its
+/// transitive imports.
+Builder cacheBuilder(BuilderOptions _) => CacheBuilder();
