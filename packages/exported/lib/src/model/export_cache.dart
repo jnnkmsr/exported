c07597f1b0b2c8191fa3a91188efda8a07eef06b
Jannik Möser <dev@jannik-moeser.de>
2025-03-03 00:08:28 +0100

    docs: add doc comment to `ExportCache`
    Refs: #13

--- packages/exported/lib/src/model/export_cache.dart
+++ packages/exported/lib/src/model/export_cache.dart
@@ -1,19 +1,35 @@
 import 'package:collection/collection.dart';
+import 'package:exported/src/builder/cache_builder.dart';
+import 'package:exported/src/builder/exported_builder.dart';
+import 'package:exported/src/model/barrel_file.dart';
 import 'package:exported/src/model/export.dart';
 import 'package:exported/src/model/export_uri.dart';
 import 'package:exported/src/model/tag.dart';
 
-// TODO[ExportCache]: Documentation
-
+/// Stores [Export] instances grouped by [Tag].
+///
+/// Created by [CacheBuilder] from annotated elements, and stored as JSON per
+/// library in the build cache. Read by [ExportedBuilder] and merged with the
+/// [Export]s from the builder options to create a combined package cache, from
+/// which generated barrel files are filled with exports matching their tags.
 class ExportCache {
+  /// Creates an [ExportCache] containing the given [exports].
+  ///
+  /// Used by [CacheBuilder] to create a cache from annotated elements in a
+  /// single library.
   ExportCache(Iterable<Export> exports) : _exportsByTag = {} {
     add(exports);
   }
 
+  /// Creates a new [ExportCache] by merging multiple [caches].
+  ///
+  /// Used by [ExportedBuilder] to create a combined package cache from all
+  /// library caches stored in the build cache.
   ExportCache.merged(Iterable<ExportCache> caches) : _exportsByTag = {} {
     caches.forEach(merge);
   }
 
+  /// Restores an [ExportCache] from [json].
   ExportCache.fromJson(Map json) {
     final exports = json.cast<String, List>().map((tag, exportsJson) {
       final exports = exportsJson.cast<Map>().map(Export.fromJson);
@@ -22,8 +38,14 @@ class ExportCache {
     _exportsByTag = exports;
   }
 
+  /// Exports grouped by tag and URI.
+  ///
+  /// When [add]ing exports, they are split into single-tag groups and merged
+  /// by URI, allowing to merge the correct export filter per barrel file.
   late final Map<Tag, Map<ExportUri, Export>> _exportsByTag;
 
+  /// Returns all exports matching the tags of the given [file], merging and
+  /// sorting by URI.
   Iterable<Export> matchingExports(BarrelFile file) {
     final exports = <ExportUri, Export>{};
     final matchingTags = file.tags == Tags.none
@@ -37,6 +59,7 @@ class ExportCache {
     return exports.values.sorted();
   }
 
+  /// Adds [exports] to the cache, grouping them by tag and merging by URI.
   void add(Iterable<Export> exports) {
     for (final export in exports) {
       for (final tag in export.tags) {
@@ -45,10 +68,12 @@ class ExportCache {
     }
   }
 
+  /// Merges [other] into this cache, merging exports by tag and URI.
   void merge(ExportCache other) => other._exportsByTag.forEach(
         (tag, exportsByUri) => _exportsByTag.putIfAbsent(tag, () => {}).merge(exportsByUri),
       );
 
+  /// Converts this [ExportCache] to JSON for storage in the build cache.
   Map toJson() => _exportsByTag.map(
         (tag, exportsByUri) => MapEntry(
           tag,
@@ -58,7 +83,11 @@ class ExportCache {
 }
 
 extension on Map<ExportUri, Export> {
+  /// Adds the given [export] to this map, merging if an export with the same
+  /// URI already exists.
   void add(Export export) => update(export.uri, export.merge, ifAbsent: () => export);
 
+  /// Merges all exports from [other] into this map by calling [add] to either
+  /// add or merge by URI.
   void merge(Map<ExportUri, Export>? other) => other?.values.forEach(add);
 }
